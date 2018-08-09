2fcb743cccbc471e1d59a7727d9147181e5825fe
Jacob Doering-Powell <jacobdoeringpowell@gmail.com>
2018-08-09 00:43:32 -0700

    rendering the main template

--- cape_eval_app/cape_app/__init__.py
+++ cape_eval_app/cape_app/__init__.py
@@ -1,36 +1,10 @@
-import os 
-from flask import Flask
+from flask import Flask, render_template
 
-def create_app(test_config=None):
-	#create and configure app
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_mapping(
-		SECRET_KEY='dev',
-		DATABASE=os.path.join(app.instance_path, 'data.db'),
-	)
+app = Flask(__name__)
 
-	if test_config is None:
-		#load the instance config, if it exists, when not testing
-		app.config.from_pyfile('config.py', silent=True)
+@app.route('/')
+def searchpage():
+	return render_template("main.html")
 
-	else:
-		#load the test config if passed in
-		app.config.from_mapping(test_config)
-
-	#make sure instance folder exists
-	try:
-		os.makedirs(app.instance_path)
-
-	except OSError:
-		pass
-
-	#a simple page that says hello
-
-	@app.route('/hello')
-	def hello():
-		return 'hello, world!'
-
-	from . import db
-	db.init_app(app)
-
-	return app
+if __name__ == "__main__":
+	app.run()
