454fb0a21c20d40adf16130104d72fdd615bdbe7
Jacob Doering-Powell <jacobdoeringpowell@gmail.com>
2018-08-26 22:39:03 -0700

    added modular functionaility for different professors

--- cape_eval_app/cape_app/__init__.py
+++ cape_eval_app/cape_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, request, url_for, redirect
 import sqlite3
+import database as db
 import sys
-from secret import install_secret_key
 
 app = Flask(__name__)
 app.secret_key = "super secret key" #change before production
@@ -20,48 +20,35 @@ def searchpage():
 			professor = request.form['professor']
 			classCode = request.form['classCode']
 
+			data = db.database('./data.db')
+
+			professorC = data.cleanInputName(professor)
+
+			profData = data.querySpecificProfessor(professorC[0], professorC[1], classCode)
+
+			if len(profData) > 0:
+
+				labels = data.queryTerm(professorC[0], professorC[1], classCode)
+				recommendI = data.queryRecI(professorC[0], professorC[1], classCode)
+				recommendC = data.queryRecC(professorC[0], professorC[1], classCode)
+
+
+				return render_template("display.html", professor=professor, classCode=classCode, labels=labels, recommendI=recommendI, recommendC=recommendC)
+
+			else:
+
+				return render_template("display.html", professor="fail", classCode=classCode)
+
+			data.close()
+
+			#logic for database queries will follow here
 
 			flash(classCode)
 			flash(professor)
-
-
-			#TODO make JSON serializable and check inputs
-			'''
-			if request.form['submit'] is 'search':
-				professor = request.form['professor']
-				classCode = request.form['classCode']
-					
-				#split the post for the professor 
-				professor = professor.split('%2C+')
-
-				professor = professor[0] + ', ' + professor[1]
-
-				#split the post for the class code
-
-				classCode = classCode.split('+')
-
-				classCode = classCode[0] + ' ' + classCode[1]
-
-				#flash(professor)
-				#flash(classCode)
-
-				
-				
-				conn = sqlite3.connect('data.db')
-				c = conn.cursor()
-				c.execute()
-				
-
-					#this is where I would check if professor and classCode match
-					#anything is the db
-
-					#just a test right now
-					#if professor is 'philip' and classCode is 'CSE 12':
-
-				#return redirect(url_for('display'))
-			'''
 			
-			return redirect(url_for('display'))
+			#return redirect(url_for('display'))
+			return render_template("display.html", professor=professor, classCode=classCode)
+		
 		else:
 		
 			return redirect('/')
