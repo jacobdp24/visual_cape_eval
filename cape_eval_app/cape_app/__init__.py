774e7402c06db3ad178f40222549ff8d85f5baef
Jacob Doering-Powell <jacobdoeringpowell@gmail.com>
2018-09-19 23:36:40 -0700

    just need to fix logic for the two forms

--- cape_eval_app/cape_app/__init__.py
+++ cape_eval_app/cape_app/__init__.py
@@ -1,35 +1,69 @@
-from flask import Flask, render_template, flash, request, url_for, redirect
+from flask import Flask, render_template, flash, request, url_for, redirect, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField
 import sqlite3
 import database as db
 import sys
 
+
 app = Flask(__name__)
-app.secret_key = "super secret key" #change before production
+app.config['SECRET_KEY'] = "super secret key" #change before production
 
-@app.route('/')
+class classForm(FlaskForm):
+	findClass = StringField('findClass')
+
+class searchForm(FlaskForm):
+
+	professor = StringField('professor', description="test") #add the validator parameter
+	classCode = StringField('classCode')
+
+'''@app.route('/')
 def mainPage():
-	return render_template("main.html")
+	return render_template("main.html")'''
 
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def searchpage():
-
+	#you are at /list/ and that needs to be fixed
 	try:
 		
-		if request.method == "POST":
+		form = searchForm(request.form)
 
-			professor = request.form['professor']
-			classCode = request.form['classCode']
+		#and form.validate()
+
+		if request.method == "POST":
 
 			data = db.database('./data.db')
 
+
+			#professor = request.form['professor']
+			#classCode = request.form['classCode']
+			#exClass = request.form['exClassCode']
+
+			professor = request.form.get('professor')
+			professor = request.form.get('classCode')
+			exClass = request.form.get('exClassCode')
+
+			teacherList = data.queryProfessorNames(exClass)
+
+			if len(professorC) > 0:
+				profData = data.querySpecificProfessor(professorC[0], professorC[1], classCode)
+
 			professorC = data.cleanInputName(professor)
 
-			profData = data.querySpecificProfessor(professorC[0], professorC[1], classCode)
+			profData = []
+
+			#figure out logic to handle the forms here
+			if not teacherList:
+				#put in fail page for the main_list here
+				return render_template('main_list.html', teacherList='nope')
+			elif len(teacherList) > 0:
+				return render_template('main_list.html', teacherList=teacherList)
+
 
 			if len(profData) > 0:
 
-				first = professor[1]
-				last = professor[0]
+				first = professorC[1]
+				last = professorC[0]
 				labels = data.queryTerm(professorC[0], professorC[1], classCode)
 				recommendI = data.queryRecI(professorC[0], professorC[1], classCode)
 				recommendC = data.queryRecC(professorC[0], professorC[1], classCode)
@@ -37,34 +71,49 @@ def searchpage():
 				avgE = data.queryAvgExpected(professorC[0], professorC[1], classCode)
 				avgR = data.queryAvgReceived(professorC[0], professorC[1], classCode)
 
-
-
 				return render_template("display.html", professor=professor, classCode=classCode, first=first, last=last, labels=labels, recommendI=recommendI, recommendC=recommendC, studyHours=studyHours, avgR=avgR, avgE=avgE)
 
-			else: #add in fail page
+				#add in fail page
 
-				return render_template("display.html", professor="fail", classCode=classCode)
+
+				#form is out
+			return render_template("main.html")
 
 			data.close()
 
 			#logic for database queries will follow here
 
-			flash(classCode)
-			flash(professor)
-			
-			#return redirect(url_for('display'))
-			return render_template("display.html", professor=professor, classCode=classCode)
+			#flash(classCode)
+			#flash(professor)
 		
-		else:
 		
-			return redirect('/')
+		#form is out 
+		return render_template('main.html')
 	
 	except Exception as e:
 		flash(e)
-		print(str(e))
+		#form is out
 		return render_template("main.html")
+		
+'''@app.route('/list/', methods=["GET","POST"])
+def listClasses():
 
-	
+	form = classForm(request.POST)
+
+	if request.method == "POST":
+
+		exClass = request.form['exClassCode']
+
+		print(exClass, file=sys.stdout)
+
+		if exClass != None:
+
+			return render_template('main_list.html', form=form, exClass=exClass)
+
+		else:
+
+			redirect(url_for('/'))'''
+
 
 @app.route('/display/')
 def display():
