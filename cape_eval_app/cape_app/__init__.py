61dd2f8924f7b5c1093aab1eebd59230dc84894c
Jacob Doering-Powell <jacobdoeringpowell@gmail.com>
2018-09-21 23:36:20 -0700

    fixed some logic, still needs to be cleaned

--- cape_eval_app/cape_app/__init__.py
+++ cape_eval_app/cape_app/__init__.py
@@ -26,7 +26,7 @@ def searchpage():
 	#you are at /list/ and that needs to be fixed
 	try:
 		
-		form = searchForm(request.form)
+		#form = searchForm(request.form)
 
 		#and form.validate()
 
@@ -40,30 +40,31 @@ def searchpage():
 			#exClass = request.form['exClassCode']
 
 			professor = request.form.get('professor')
-			professor = request.form.get('classCode')
+			classCode = request.form.get('classCode')
 			exClass = request.form.get('exClassCode')
+
+			classCode = str(classCode)
+			exClass = str(exClass)
+
+			classCode = classCode.upper()
+			exClass = exClass.upper()
 
 			teacherList = data.queryProfessorNames(exClass)
 
-			if len(professorC) > 0:
-				profData = data.querySpecificProfessor(professorC[0], professorC[1], classCode)
-
 			professorC = data.cleanInputName(professor)
 
-			profData = []
+			profData = None
 
-			#figure out logic to handle the forms here
-			if not teacherList:
-				#put in fail page for the main_list here
-				return render_template('main_list.html', teacherList='nope')
-			elif len(teacherList) > 0:
-				return render_template('main_list.html', teacherList=teacherList)
-
+			if len(professorC) > 0:
+				profData = data.querySpecificProfessor(professorC[0], professorC[1], classCode)
+			
 
 			if len(profData) > 0:
 
 				first = professorC[1]
 				last = professorC[0]
+				combine = first + ' ' + last
+
 				labels = data.queryTerm(professorC[0], professorC[1], classCode)
 				recommendI = data.queryRecI(professorC[0], professorC[1], classCode)
 				recommendC = data.queryRecC(professorC[0], professorC[1], classCode)
@@ -71,17 +72,26 @@ def searchpage():
 				avgE = data.queryAvgExpected(professorC[0], professorC[1], classCode)
 				avgR = data.queryAvgReceived(professorC[0], professorC[1], classCode)
 
-				return render_template("display.html", professor=professor, classCode=classCode, first=first, last=last, labels=labels, recommendI=recommendI, recommendC=recommendC, studyHours=studyHours, avgR=avgR, avgE=avgE)
+				return render_template("display.html", professor=combine, classCode=classCode, first=first, last=last, labels=labels, recommendI=recommendI, recommendC=recommendC, studyHours=studyHours, avgR=avgR, avgE=avgE)
+				#return render_template('main_list.html', teacherList=first + ' ' + last)
 
 				#add in fail page
 
+
+			#figure out logic to handle the forms here
+			if not teacherList and profData != None:
+				#put in fail page for the main_list here
+				return render_template('main_list.html', teacherList='professor or class not found')
+			elif len(teacherList) > 0:
+				return render_template('main_list.html', teacherList=teacherList)
+			
+
+			
 
 				#form is out
 			return render_template("main.html")
 
 			data.close()
-
-			#logic for database queries will follow here
 
 			#flash(classCode)
 			#flash(professor)
@@ -90,29 +100,26 @@ def searchpage():
 		#form is out 
 		return render_template('main.html')
 	
+	
+	#except AttributeError:
+	#	return render_template('main_list.html', teacherList=classCode)
+
+
+	except IndexError:
+		return render_template('main_list.html', teacherList='when do I get Index errors')
+
+	except TypeError:
+		if not teacherList:
+			return render_template('main_list.html', teacherList='class not found in lower search bar')
+
+		else:
+			return render_template('main_list.html', teacherList=teacherList)
+
 	except Exception as e:
 		flash(e)
 		#form is out
 		return render_template("main.html")
 		
-'''@app.route('/list/', methods=["GET","POST"])
-def listClasses():
-
-	form = classForm(request.POST)
-
-	if request.method == "POST":
-
-		exClass = request.form['exClassCode']
-
-		print(exClass, file=sys.stdout)
-
-		if exClass != None:
-
-			return render_template('main_list.html', form=form, exClass=exClass)
-
-		else:
-
-			redirect(url_for('/'))'''
 
 
 @app.route('/display/')
